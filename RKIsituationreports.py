5b702254bb4ce413e1bd6119d1e8b89dc5701ba3
matthias linden <matthiaslinden@Matthias-MacPro.fritz.box>
2020-04-23 04:17:26 +0200

    Retrieval of file in github-repro seems to work

--- RKIsituationreports.py
+++ RKIsituationreports.py
@@ -2,27 +2,30 @@
 #coding:utf-8
 
 import datetime
-import urllib
+from urllib import request
 import os
 
 class RKIcachedreports(object):
     """Encapsualtes data handling from local cache, try to fetch newer version"""
-    def __init__(self,source=None,location=None):
+    def __init__(self,source=None,location=None,force_download=False):
         """"""
         self.started = datetime.datetime.now()
         self.storage_location = "data/RKIsituationreports"
         self.filename = "latest_report.csv"
         # Handle source argument
         if source == None:
-            source = "https://github.com/matthiaslinden/RKI_COVID_SituationReport/"+self.storage_location+"/"+self.filename
+            source = "https://github.com/matthiaslinden/RKI_COVID_SituationReport/raw/master/"+self.storage_location+"/"+self.filename
         self.source_url = source
         # Handle (local)location argument
         if location != None:
             self.storage_location = location
         # Try to open local copy, if outdated --> fetch
-        is_latest = self.OpenLocal()
+        if not force_download:
+            is_latest = self.OpenLocal()
+        else:
+            is_latest = False
         if not is_latest:
-            if self.Fetch():
+            if not self.Fetch():
                 print("Unable to retrieve updated source")
         
     def Fetch(self,url=None):
@@ -30,13 +33,16 @@ class RKIcachedreports(object):
         if url == None:
             url = self.source_url
         success = False
-        latest_data = urllib.request.urlopen(url)
-        with open(self.storage_location+"/"+self.filename,"w+") as f:
-            f.write(datetime.datetime.now().isoformat()+"\n")
+        remote_file = request.urlopen(url)
+        creation_date = datetime.datetime.fromisoformat(remote_file.readline().decode("utf-8")[:-1])
+        latest_data = remote_file.read()
+        with open(self.storage_location+"/"+self.filename,"wb+") as f:
+            f.write((datetime.datetime.now().isoformat()+"\n").encode("utf-8"))
             f.write(latest_data)
             success = True
         if success:
-            self.Parse(latest_data)
+            print("Fetched remote situation report, as of %s"%(creation_date.isoformat()))
+            self.Parse(latest_data.decode("utf-8"))
         return success
     
     def OpenLocal(self,location=None):
@@ -49,20 +55,24 @@ class RKIcachedreports(object):
                 if age > datetime.timedelta(hours = 4):
                     return False
                 else:
+                    print("Use locally cached situation report last fetched %s ago"%age)
                     self.Parse(f.read())
                     return True
             return False
     
     def Parse(self,file_data):
-        """Parse the inputdata"""
+        """Parse the inputdata -- Replace in derived class"""
         print("Parse",file_data)
         
 class RKIsituationreports(RKIcachedreports):
-    def __init__(self,source="default"):
-        baseargs = {}
+    def __init__(self,source="default",**kwargs):
+        baseargs = kwargs
         if source != "default":
             baseargs["source"] = source
         super(RKIsituationreports,self).__init__(**baseargs)
+        
+    def Parse(self,file_data):
+        print("Parse situation report",file_data)
 
 def main():
     situationreports = RKIsituationreports()
